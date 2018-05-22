317d4fffc8b66ede5cfe6b75f88bb49756f67252
github username <github email address>
2018-05-22 14:28:47 -0400

    apollo-graphql-setup

--- src/containers/YelperContainer.js
+++ src/containers/YelperContainer.js
@@ -1,19 +1,72 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchBusiness } from '../actionreducer/actions/action'
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
 
 
 
 class YelperContainer extends React.Component {
 
-  componentDidMpunt(){
-    this.props.fetchBusiness()
-  }
+  // componentDidMount(){
+  //   //this.props.fetchBusiness()
+  //
+  //   <Query
+  //     query={gql`
+  //      {
+  //        rates(currency: "USD") {
+  //          currency
+  //          rate
+  //        }
+  //      }
+  //    `}
+  //   >
+  //   {
+  //     ({ loading, error, data }) => {
+  //
+  //       if (loading) return <p>Loading...</p>;
+  //       if (error) return <p>Error :(</p>;
+  //
+  //       return data.rates.map(({ currency, rate }) => (
+  //         <div key={currency}>
+  //           <p>{`${currency}: ${rate}`}</p>
+  //         </div>
+  //       ));
+  //     }
+  //   }
+  // </Query>
+  // }
 
   render(){
 
     return(
       <div>
+         <Query
+        query={gql`
+         {
+           rates(currency: "USD") {
+             currency
+             rate
+           }
+         }
+       `}
+      >
+      {
+        ({ loading, error, data }) => {
+
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+
+          return data.rates.map(({ currency, rate }) => (
+            <div key={currency}>
+              <p>{`${currency}: ${rate}`}</p>
+            </div>
+          ));
+        }
+      }
+    </Query>
+
         YELPERJOBS
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -7,12 +7,35 @@ import { createStore, applyMiddleware } from 'redux';
 import  thunk  from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { yelperReducer} from './actionreducer/reducers/reducer'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from "react-apollo";
+// import gql from "graphql-tag";
+
+
+const client = new ApolloClient({
+  uri:"https://w5xlvm3vzz.lp.gql.zone/graphql"
+})
+//
+// client
+//   .query({
+//     query: gql`
+//       {
+//         rates(currency: "USD") {
+//           currency
+//           rate
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
 const store = createStore(yelperReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </Router>, document.getElementById('root'));
